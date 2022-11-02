1d4a7d9093af2f0315056cd18d03fe189d913469
Magali Perche <magali.per@hotmail.fr>
2022-11-02 13:50:10 +0100

    add doctype

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    
+
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
@@ -12,4 +24,6 @@ $daisy->setEyes('$$')
 
 echo $daisy . PHP_EOL;
 ?>
-
+</pre>
+</body>
+</html>
